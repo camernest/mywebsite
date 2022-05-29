d716f908675634cf2b94906ae9c018362c1417fe
Cameron Ernest <cameron.ernest@outlook.com>
2022-05-29 02:38:32 -0400

    Typing updated homepage

---
+++ assets/js/keystroke.js
@@ -0,0 +1,24 @@
+document.getElementById("textId").addEventListener("keydown", myFunction);
+
+function myFunction() {
+
+  switch (event.key) {
+    case "ArrowDown":
+    	console.log("ArrowDown");
+      break;
+    case "ArrowUp":
+      console.log("ArrowUp");
+      break;
+    case "ArrowLeft":
+      console.log("ArrowLeft");
+      break;
+    case "ArrowRight":
+      console.log("ArrowRight");
+      break;
+    default:
+      console.log(event.key, event.keyCode);
+      return; 
+  }
+
+  event.preventDefault();
+}
